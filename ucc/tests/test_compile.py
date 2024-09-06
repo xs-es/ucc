a41680d9f5055c899390a992a42e0717d7ea69d4
jordandsullivan <jordan@unitary.fund>
2024-09-06 00:37:23 -0700

    Add qiskit and cirq tests for high level compiler, add gates to corresponding test circuits

--- ucc/tests/test_compile.py
+++ ucc/tests/test_compile.py
@@ -4,7 +4,21 @@ from ucc import compile
 from pytket import Circuit as TketCircuit
 from cirq import Circuit as CirqCircuit
 from qiskit import QuantumCircuit as QiskitCircuit
+from cirq import H, CNOT, LineQubit
 
 def test_tket_compile():
     circuit = TketCircuit(2)
+    circuit.H(0)
+    circuit.CX(0, 1)
+    compile(circuit, qasm_version='2', return_format='original')
+
+def test_qiskit_compile():
+    circuit = QiskitCircuit(2)
+    circuit.h(0)
+    circuit.cx(0, 1)
+    compile(circuit, qasm_version='2', return_format='original')
+
+def test_cirq_compile():
+    qubits = LineQubit.range(2)
+    circuit = CirqCircuit(H(qubits[0]), CNOT(qubits[0], qubits[1]))
     compile(circuit, qasm_version='2', return_format='original')
