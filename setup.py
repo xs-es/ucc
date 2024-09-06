0cd4dd09bc9bf20c539757a0388f48cf8b723036
jordandsullivan <jordan@unitary.fund>
2024-09-05 22:04:43 -0700

    add setup.py boilerplate for UCC

---
+++ setup.py
@@ -0,0 +1,39 @@
+from setuptools import setup, find_packages
+
+# Read the contents of your README file
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="ucc",  # Package name
+    version="0.1.0",  # Initial release version
+    author="Jordan Sullivan",
+    author_email="jordan@unitary.fund",
+    description="Unitary Compiler Collection: A quantum circuit interface and compiler for multiple quantum frameworks",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/unitaryfund/ucc",  # Repository URL
+    packages=find_packages(),  # Automatically find and include all packages in the project
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",  # If using MIT License, adjust if needed
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.7",  # Minimum Python version required
+    install_requires=[
+        "qiskit>=0.41.0",  # Required version of Qiskit
+        "cirq>=0.13.0",  # Required version of Cirq
+        "pytket>=1.3.0",  # Required version of PyTKET
+    ],
+    extras_require={
+        "dev": [
+            "pytest>=6.0",  # Testing framework
+            "pytest-cov>=2.10",  # Coverage plugin for pytest
+        ],
+    },
+    entry_points={
+        "console_scripts": [
+            "ucc=ucc.__main__:main",  # Command-line entry point
+        ],
+    },
+)
