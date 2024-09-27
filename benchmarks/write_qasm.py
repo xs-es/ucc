b9554bad46e99e4d5318fef14c678b9a20eabffc
jordandsullivan <jordan@unitary.fund>
2024-09-27 14:53:30 -0700

    Add decomposed OpenQASM circuits. Overall efficiency of UCC greatly decreased.

--- benchmarks/write_qasm.py
+++ benchmarks/write_qasm.py
@@ -3,12 +3,15 @@ from qbraid.transpiler import transpile as translate
 from qbraid.programs.alias_manager import get_program_type_alias
 from qiskit import transpile as qiskit_transpile
 
-def write_qasm(circuit, filename, version='2'):
+def write_qasm(circuit, filename, version='2', basis_gates=False):
     qiskit_circuit = translate(circuit, 'qiskit')
-    decomp_circuit = qiskit_transpile(
-        qiskit_circuit, 
-        basis_gates=['rz', 'rx', 'ry', 'h', 'cx'], 
-        optimization_level=0)
+    if basis_gates:
+        decomp_circuit = qiskit_transpile(
+            qiskit_circuit, 
+            basis_gates=basis_gates, 
+            optimization_level=0)
+    else:
+        decomp_circuit = qiskit_circuit
 
     # Generate QASM string
     qasm_string = translate(decomp_circuit, 'qasm' + version)
