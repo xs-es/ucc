0c8eb697252792fd34bdbab76d2148d9e808d410
Misty-W <82074193+Misty-W@users.noreply.github.com>
2024-11-19 10:07:11 -0800

    Use `GatesinBasis` analysis pass for test

--- ucc/tests/test_compile.py
+++ ucc/tests/test_compile.py
@@ -1,15 +1,13 @@
 import pytest
+from benchmarks.circuits import qcnn_circuit, random_clifford_circuit
 from cirq import CNOT
 from cirq import Circuit as CirqCircuit
 from cirq import H, LineQubit
 from pytket import Circuit as TketCircuit
 from qiskit import QuantumCircuit as QiskitCircuit
-from qiskit.circuit.equivalence_library import SessionEquivalenceLibrary as sel
 from qiskit.converters import circuit_to_dag
-
-from benchmarks.circuits import qcnn_circuit, random_clifford_circuit
+from qiskit.transpiler.passes import GatesInBasis
 from ucc import compile
-from ucc.transpiler_passes.basis_translator import BasisTranslator
 from ucc.transpilers.ucc_defaults import UCCDefault1
 
 
@@ -36,31 +34,15 @@ def test_tket_compile():
     assert isinstance(result_circuit, TketCircuit)
 
 
-circuit_names = [
-    "qcnn_circuit",
-    "random_clifford_circuit",
-]
-
-
-@pytest.mark.parametrize("circuit_name", circuit_names)
+@pytest.mark.parametrize("circuit_function", [qcnn_circuit, random_clifford_circuit])
 @pytest.mark.parametrize("num_qubits", [4, 5, 6, 7, 8, 9, 10])
 @pytest.mark.parametrize("seed", [1, 326, 5678, 12345])
-def test_gateset_of_out_circuit(circuit_name, num_qubits, seed):
+def test_gateset_of_out_circuit(circuit_function, num_qubits, seed):
     circuit = circuit_function(num_qubits, seed)
     transpiler = UCCDefault1()
     target_basis = transpiler.target_basis
     transpiled_circuit = transpiler.run(circuit)
     dag = circuit_to_dag(transpiled_circuit)
-    qarg_indices = {qubit: index for index, qubit in enumerate(dag.qubits)}
-    basis_translator = next(
-        (t for t in transpiler.pass_manager._tasks if isinstance(t, BasisTranslator)),
-        BasisTranslator(sel, target_basis),
-    )
-    compiled_basis, qargs_local_source_basis = basis_translator._extract_basis_target(
-        dag, qarg_indices
-    )
-
-    assert (
-        set(list(zip(*compiled_basis))[0]).issubset(target_basis)
-        and not qargs_local_source_basis
-    )
+    analysis_pass = GatesInBasis(basis_gates=target_basis)
+    analysis_pass.run(dag)
+    assert analysis_pass.property_set["all_gates_in_basis"] == True
