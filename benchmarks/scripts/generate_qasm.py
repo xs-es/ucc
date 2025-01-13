9f62ec55e81c66c4dc45695a5214d15e4040987e
jordandsullivan <jordan@unitary.fund>
2025-01-13 15:00:04 -0800

    remove unused QASM generation functions

--- benchmarks/scripts/generate_qasm.py
+++ benchmarks/scripts/generate_qasm.py
@@ -5,13 +5,13 @@
 # Prepare an N-qubit GHZ state and select the all ones state.
 
 from write_qasm import write_qasm
-from cirq_circuits import VQE_ansatz, cirq_prep_select
-from qiskit_circuits import qcnn_circuit, qaoa_ising_ansatz
+from cirq_circuits import cirq_prep_select
+from qiskit_circuits import qcnn_circuit
 from numpy import log2, ceil
 
 
 # ### Prepare & Select
-num_qubits = 25
+num_qubits = 10
 target_state="1" * num_qubits
 
 circuit = cirq_prep_select(num_qubits, target_state=target_state)
@@ -23,7 +23,7 @@ circuit_name=filename,
 )
 
 # ### QCNN circuit
-num_qubits = 100
+num_qubits = 10
 num_layers = int(ceil(log2(num_qubits)))
 
 seed = 12345
@@ -34,22 +34,3 @@ filename = f"qcnn_N{num_qubits}_{num_layers}layers"
 write_qasm(circuit, 
 circuit_name=filename, 
 basis_gates=['rz', 'rx', 'ry', 'h', 'cx'])
-
-# ### Parameterized VQE ansatz
-num_qubits = 50
-num_layers = num_qubits * 2
-num_layers = num_qubits * 2
-
-circuit = VQE_ansatz(num_qubits, num_layers)
-filename = f"VQE_ansatz_N{num_qubits}_{num_layers}layers"
-
-write_qasm(circuit, circuit_name=filename, version='3')
-
-# ### QAOA neighest neighbors Ising mode
-num_qubits = 50
-num_layers = num_qubits * 3
-
-circuit = qaoa_ising_ansatz(num_qubits, num_layers)
-filename = f"QAOA_Ising_ansatz_N{num_qubits}_{num_layers}layers"
-
-write_qasm(circuit, circuit_name=filename, version='3')
