75958e2fdcc5679bb126e25298f2753c9f9dbaa1
jordandsullivan <jordan@unitary.fund>
2025-01-14 10:39:47 -0800

    Add filenames to expal run

--- benchmarks/scripts/run_benchmarks.sh
+++ benchmarks/scripts/run_benchmarks.sh
@@ -33,20 +33,27 @@ echo "Running with parallelism: $PARALLELISM"
 trap 'echo "All jobs killed"; exit' SIGINT SIGTERM
 
 # # Prepare the list of commands to run in parallel
-commands=()
-for qasm_file in "${QASM_FILES[@]}"; do
-    for compiler in "${COMPILERS[@]}"; do
-        # Combine the common folder path with the QASM file
-        full_qasm_file="${QASM_FOLDER}${qasm_file}"
+# commands=()
+# for qasm_file in "${QASM_FILES[@]}"; do
+#     for compiler in "${COMPILERS[@]}"; do
+#         # Combine the common folder path with the QASM file
+#         full_qasm_file="${QASM_FOLDER}${qasm_file}"
         
-        # Build the command, passing the results folder as an argument
-        command="python3 $(dirname "$0")/benchmark_script.py \"$full_qasm_file\" \"$compiler\" \"$RESULTS_FOLDER\""
+#         # Build the command, passing the results folder as an argument
+#         command="python3 $(dirname "$0")/benchmark_script.py \"$full_qasm_file\" \"$compiler\" \"$RESULTS_FOLDER\""
 
-        commands+=("$command")
-    done
-done
+#         commands+=("$command")
+#     done
+# done
 
-QASM_EXPVAL_FILES=() # TODO
+QASM_EXPVAL_FILES=(
+    "benchpress/qaoa_barabasi_albert_N10_3reps_basis_rz_rx_ry_cx.qasm"
+    "benchpress/qv_N100_12345_basis_rz_rx_ry_cx.qasm"
+    "benchpress/qft_N010_basis_rz_rx_ry_cx.qasm"
+    "benchpress/square_heisenberg_N9_basis_rz_rx_ry_cx.qasm"
+    "ucc/prep_select_N10_ghz_basis_rz_rx_ry_h_cx.qasm"
+    "ucc/qcnn_N10_4layers_basis_rz_rx_ry_h_cx.qasm"
+)
 for qasm_file in "${QASM_EXPVAL_FILES[@]}"; do
     for compiler in "${COMPILERS[@]}"; do
         # Combine the common folder path with the QASM file
