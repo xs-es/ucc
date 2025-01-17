081dd5764fe7e625c182a90072bbc290b015364f
nate stemen <nate@unitary.foundation>
2025-01-16 21:13:54 -0800

    uncomment first commands to run

--- benchmarks/scripts/run_benchmarks.sh
+++ benchmarks/scripts/run_benchmarks.sh
@@ -33,18 +33,18 @@ echo "Running with parallelism: $PARALLELISM"
 trap 'echo "All jobs killed"; exit' SIGINT SIGTERM
 
 # # Prepare the list of commands to run in parallel
-# commands=()
-# for qasm_file in "${QASM_FILES[@]}"; do
-#     for compiler in "${COMPILERS[@]}"; do
-#         # Combine the common folder path with the QASM file
-#         full_qasm_file="${QASM_FOLDER}${qasm_file}"
+commands=()
+for qasm_file in "${QASM_FILES[@]}"; do
+    for compiler in "${COMPILERS[@]}"; do
+        # Combine the common folder path with the QASM file
+        full_qasm_file="${QASM_FOLDER}${qasm_file}"
         
-#         # Build the command, passing the results folder as an argument
-#         command="python3 $(dirname "$0")/benchmark_script.py \"$full_qasm_file\" \"$compiler\" \"$RESULTS_FOLDER\""
+        # Build the command, passing the results folder as an argument
+        command="python3 $(dirname "$0")/benchmark_script.py \"$full_qasm_file\" \"$compiler\" \"$RESULTS_FOLDER\""
 
-#         commands+=("$command")
-#     done
-# done
+        commands+=("$command")
+    done
+done
 
 QASM_EXPVAL_FILES=(
     "benchpress/qaoa_barabasi_albert_N10_3reps_basis_rz_rx_ry_cx.qasm"
