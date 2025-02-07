8ce7b536930413a0dea36b2b59dfc33babf07ebd
Brad Chase <brad@unitary.foundation>
2025-02-07 13:40:21 -0500

    Add target gate set for cirq benchmarking:
    cirq's optimize_for_target_gateset function requires providing a
    class that compiles to a desired target gateset. None of the cirq
    provided target gatesets use the set of gates we consider for benchmarking, so
    this change adds an appropriate one,

--- benchmarks/scripts/common.py
+++ benchmarks/scripts/common.py
@@ -4,7 +4,9 @@ import os
 import pandas as pd
 import matplotlib
 from datetime import datetime
-from cirq import CZTargetGateset, optimize_for_target_gateset
+from typing import List
+from cirq import optimize_for_target_gateset
+import cirq
 from pytket.circuit import OpType
 from pytket.passes import (
     DecomposeBoxes,
@@ -108,9 +110,99 @@ def qiskit_compile(qiskit_circuit):
     )
 
 
+class BenchmarkTargetGateset(cirq.TwoQubitCompilationTargetGateset):
+    """Target gateset for compiling circuits for benchmarking.
+
+     This is modeled off cirq's CZCompilationTargetGateset, but instead:
+         * Decomposes single-qubit gates into Rz, Ry, and Rz gates versus XZPowGate.
+         * Decomposes two-qubit gates into CNOT gates versus CZPowGate.
+         * Overrides the base classes postprocess_transformers to eliminate the
+           merge_single_qubit_moments_to_phxz pass to avoid re-introducing XZPowGates.
+
+    The gate families accepted by this gateset are:
+     *  Single-Qubit Gates: `cirq.H`, `cirq.Rx`, `cirq.Ry`, `cirq.Rz`.
+     *  Two-Qubit Gates: `cirq.CNOT`
+     *  `cirq.MeasurementGate`
+    """
+
+    def __init__(self):
+        """Initializes BenchmarkTargetGateset"""
+        super().__init__(
+            cirq.H,
+            cirq.CNOT,
+            cirq.Rx,
+            cirq.Ry,
+            cirq.Rz,
+            cirq.MeasurementGate,
+            name="BenchmarkTargetGateset",
+        )
+
+    def _decompose_single_qubit_operation(
+        self, op: cirq.Operation, _
+    ) -> cirq.OP_TREE:
+        if not cirq.protocols.has_unitary(op):
+            return NotImplemented
+
+        mat = cirq.unitary(op)
+
+        pre_phase, rotation, post_phase = (
+            cirq.linalg.deconstruct_single_qubit_matrix_into_angles(mat)
+        )
+        return [
+            cirq.rz(pre_phase).on(op.qubits[0]),
+            cirq.ry(rotation).on(op.qubits[0]),
+            cirq.rz(post_phase).on(op.qubits[0]),
+        ]
+
+    def _decompose_two_qubit_operation(
+        self, op: cirq.Operation, _
+    ) -> cirq.OP_TREE:
+        if not cirq.has_unitary(op):
+            return NotImplemented
+        mat = cirq.unitary(op)
+        q0, q1 = op.qubits
+        naive = cirq.two_qubit_matrix_to_cz_operations(
+            q0, q1, mat, allow_partial_czs=False
+        )
+        temp = cirq.map_operations_and_unroll(
+            cirq.Circuit(naive),
+            lambda op, _: (
+                [
+                    cirq.H(op.qubits[1]),
+                    cirq.CNOT(*op.qubits),
+                    cirq.H(op.qubits[1]),
+                ]
+                if op.gate == cirq.CZ
+                else op
+            ),
+        )
+        return cirq.merge_k_qubit_unitaries(
+            temp,
+            k=1,
+            rewriter=lambda op: self._decompose_single_qubit_operation(op, -1),
+        ).all_operations()
+
+    @property
+    def postprocess_transformers(self) -> List["cirq.TRANSFORMER"]:
+        """List of transformers which should be run after decomposing individual operations."""
+        processors: List["cirq.TRANSFORMER"] = [
+            cirq.transformers.drop_negligible_operations,
+            cirq.transformers.drop_empty_moments,
+        ]
+        if not self._preserve_moment_structure:
+            processors.append(cirq.transformers.stratified_circuit)
+        return processors
+
+    def __repr__(self) -> str:
+        return "BenchmarkTargetGateset()"
+
+
 # Cirq compilation
 def cirq_compile(cirq_circuit):
-    return optimize_for_target_gateset(cirq_circuit, gateset=CZTargetGateset())
+    res = optimize_for_target_gateset(
+        cirq_circuit, gateset=BenchmarkTargetGateset()
+    )
+    return res
 
 
 def get_n_qubit_gateset(
