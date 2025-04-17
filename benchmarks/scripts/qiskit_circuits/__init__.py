493e9a9552b21ad315254bc9d133ec8aecddf9e8
Brad Chase <brad@unitary.foundation>
2025-04-17 15:32:38 -0400

    Remove old benchmarking code and results

--- benchmarks/scripts/qiskit_circuits/__init__.py
+++
@@ -1,6 +0,0 @@
-from .circuits import (
-    qcnn_circuit as qcnn_circuit,
-    VQE_ansatz as VQE_ansatz,
-    qaoa_ising_ansatz as qaoa_ising_ansatz,
-    random_clifford_circuit as random_clifford_circuit,
-)
